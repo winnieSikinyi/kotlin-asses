a60fc6b8120011ed5821b1c9eca4e45436ea9d62
winnieSikinyi <winniesikinyi@gmail.com>
2023-03-09 08:51:36 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,62 @@
+fun main() {
+
+ Dtdobbie()
+   return println()
+    coffee()
+sentence("Newyork","Amsterdeerm")
+    calculator(34,58,54.533,12)
+
+
+
+}
+
+//Qn1
+fun sentence(str1:String,str2:String): String {
+    var words = str1
+   return words
+    println(words.indexOf(str1))
+
+}
+
+//2
+fun Dtdobbie(): Array<Int>{
+    var tyres = arrayOf(2,5,8,12,14)
+    return tyres.()
+    tyres.max()
+    tyres.min()
+    tyres.average()
+}
+
+
+//3
+fun coffee(): String{
+ var kahawa="green,cocoa,nescafe,"
+    var coffee = kahawa.split(",")
+    return coffee()
+//4
+}
+    class calculator (var Add:Int,var Difference:Int,var Div:Double,var Multiply:Int){
+      fun add(){
+
+
+      }
+    fun diff(){
+
+    }
+    fun div(){
+
+    }
+        fun mult(){
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+
